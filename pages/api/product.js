21db6d31513f2886ef3757e739743de6505c4ad1
Naorid <ashur4d@gmail.com>
2022-04-29 19:04:23 +0200

    Added .env and /api/products route connected to shopify

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,20 +1,20 @@
 import nc from 'next-connect'
 import cors from 'cors'
-import products from '../../src/data/data'
 
-const handler = nc()
-    .use(cors())
-    // GET returns all products
-    .get((req, res) => {
-        res.json({data: products})
-    })
-    // POST add a product
-    .post((req, res) => {
-        const id = Date.now()
-        const product = {...req.body, id}
+//import products from '../../src/data/data'
 
-        products.push(product)
-        res.json({data: product})
-    })
+require('dotenv').config()
 
-export default handler
+export default async function handler(req, res) {
+    const rawProducts = await fetch(
+        `${process.env.SHOPIFY_DOMAIN}/admin/api/${process.env.API_VERSION}/products.json`,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Access-Token': process.env.ACCESS_TOKEN
+            }
+        }
+    )
+    const products = await rawProducts.json()
+    res.json({data: products})
+}
