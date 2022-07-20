b30f62e991b9a008c28cd415e68f394589082ad9
nathan.prats <nathan.prats@octo.com>
2022-07-20 18:25:51 +0200

    start delivery integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,8 @@ function Header() {
         <Container>
             <Box></Box>
             <img src="/assets/octobook.png" alt="" id="octobook"></img>
-            <Button colorScheme="linkedin" onClick={() => removeCart()}>Remove Cart</Button>
+            <Button bgColor={"#00b0cb"} onClick={() => removeCart()}>Remove Cart</Button>
+            <a href='http://127.0.0.1:5500/octobook-delivery/front/landing.html'>Commandes</a>
         </Container>
     )
 }
